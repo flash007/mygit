fffb48ce2a958b780c01d9ed8df9aa05c8a22a98
flash007 <chengleikai@gmail.com>
2013-07-28 16:54:59 +0800

    test

---
+++ src/com/flash/lucene/TestLucence.java
@@ -0,0 +1,12 @@
+package com.flash.lucene;
+
+import org.junit.Test;
+
+public class TestLucence {
+
+	@Test
+	public void testIndex(){
+		HelloLucence hl = new HelloLucence();
+		hl.index();
+	}
+}
